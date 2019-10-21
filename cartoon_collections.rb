419695d53beb8afe90e411ee1cb27009f5bc45e3
Wills Blake <richwblake@gmail.com>
2019-10-21 00:29:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,5 @@ end
 def find_the_cheese( snack )
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snack.find { |item|
-    if cheese_types.include?([item]) 
-      return item
-    end
-  }
+  snack.find { |item|  } 
 end
